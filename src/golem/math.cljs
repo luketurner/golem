9716dcf0ff947a4f3fe8cf9c390ef0710fe399e3
Luke Turner <github@luketurner.org>
2018-09-05 18:46:34 -0700

    Delete files from gh-pages branch

--- src/golem/math.cljs
+++
@@ -1,7 +0,0 @@
-(ns golem.math
-  (:require [cljs.spec.alpha :as s]))
-
-(s/def ::coord (s/tuple int? int?))
-
-(defn ceil [x] (.ceil js/Math x))
-(defn floor [x] (.floor js/Math x))
--- src/golem/util.cljs
+++
@@ -1,8 +0,0 @@
-(ns golem.util)
-
-
-(defn parse-int
-  ([string] (parse-int string nil))
-  ([string default]
-   (let [parsed (js/parseInt string 10)]
-     (if (js/isNaN parsed) default parsed))))
