7657abd8b501ac1d30402bff9f905abef0f7fdff
Luke Turner <github@luketurner.org>
2018-09-09 10:40:03 -0700

    Move clear-interval! and set-interval! to util

--- src/golem/update_loop.cljs
+++ src/golem/update_loop.cljs
@@ -1,37 +1,34 @@
 (ns golem.update_loop
   (:require [reagent.ratom :refer [run! cursor]]
             [golem.handlers :refer [set-interval! clear-interval!]]
-            [cljs.spec.alpha :as s]))
+            [cljs.spec.alpha :as s]
+            [golem.util :as util]))
 
-(s/def ::update-loop (s/keys :req-un []))
 (s/def ::interval pos-int?)
+(s/def ::interval-id (s/nilable int?))
 (s/def ::enabled boolean?)
+(s/def ::update-loop (s/keys :req-un [::interval
+                                      ::enabled
+                                      ::interval-id]))
 
 (def default-state {:interval 100
-                    :enabled  true})
+                    :enabled  true
+                    :interval-id nil})
 
+;; Getter functions
 (defn is-enabled? [!db] @(cursor !db [:update-loop :enabled]))
 (defn get-rate [!db] @(cursor !db [:update-loop :interval]))
 
-(defn disable! [!db]
-  (swap! !db assoc-in [:update-loop :enabled] false))
-
-(defn enable! [!db]
-  (swap! !db assoc-in [:update-loop :enabled] true))
-
-(defn toggle! [!db]
-  (swap! !db update-in [:update-loop :enabled] not))
-
-(defn inc-rate! [!db ms]
-  (swap! !db update-in [:update-loop :interval] #(max 0 (+ % ms))))
-
-(defn with-timer [tag func]
-  #(time (func)))
+;; Updater functions
+(defn disable! [!db] (swap! !db assoc-in [:update-loop :enabled] false))
+(defn enable! [!db] (swap! !db assoc-in [:update-loop :enabled] true))
+(defn toggle! [!db] (swap! !db update-in [:update-loop :enabled] not))
+(defn inc-rate! [!db ms] (swap! !db update-in [:update-loop :interval] #(max 0 (+ % ms))))
 
 (defn run-loop! [!db update-fn]
   (let [!interval (cursor !db [:update-loop :interval])
         !enabled? (cursor !db [:update-loop :enabled])]
     (run!
       (if @!enabled?
-        (set-interval! !db :update-loop @!interval update-fn)
-        (clear-interval! !db :update-loop)))))
+        (util/set-interval! !db [:update-loop :interval-id] @!interval update-fn)
+        (util/clear-interval! !db [:update-loop :interval-id])))))
--- src/golem/util.cljs
+++ src/golem/util.cljs
@@ -1,4 +1,8 @@
-(ns golem.util)
+(ns golem.util
+  "Defines functions and types that are reusable, but not large enough to warrant their own modules.
+
+   Most of the functions just wrap existing JS functions to add a more convenient API."
+  (:require [cljs.spec.alpha :as s]))
 
 (s/def ::coord (s/tuple int? int?))
 
@@ -10,3 +14,19 @@
   ([string default]
    (let [parsed (js/parseInt string 10)]
      (if (js/isNaN parsed) default parsed))))
+
+
+(defn clear-interval!
+  "If the value at `path` in `!db` is a number, call `clearInterval` on it and set it to nil."
+  [!db path]
+  (swap! !db update-in path #(when (number? %) (js/clearInterval %) nil)))
+
+(defn set-interval!
+  "Runs `function` every `interval` ms. Replaces the value at `path` in `!db` to the interval ID.
+   If the prior value was an interval ID, the corresponding interval will automatically be cleared."
+  [!db path interval function]
+  (swap! !db update-in path
+         (fn [old-interval-id]
+           (when (number? old-interval-id)
+             (js/clearInterval old-interval-id))
+           (js/setInterval function interval))))
