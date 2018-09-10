58f5caa298e474f9868c008167e06342502aedb3
Luke Turner <github@luketurner.org>
2018-09-10 15:28:01 -0700

    Better structure/align/simplify the header

--- src/golem/ui/game_controls.cljs
+++ src/golem/ui/game_controls.cljs
@@ -6,16 +6,12 @@
 
 (defn component [!db]
   [:div.game-controls
-    [:div.sidebar [:button {:on-click #(sidebar/toggle! !db)} (if (sidebar/is-open? !db) "hide" "show") " patterns"]]
-    [:div.cmds-label "Controls: "]
-    [:div.cmds
-      [:button {:on-click #(board/undo! !db)} "undo"]
-      [:button {:on-click #(update-loop/toggle! !db)} (if (update-loop/is-enabled? !db) "pause" "play")]
-      [:button {:on-click #(pattern-manager/use-selected-pattern! !db)} "reset"]]
-    [:div.rates-label "Update Rate: "]
-    [:div.rates
-      [:button {:on-click #(update-loop/inc-rate! !db 100)} "--"]
-      [:button {:on-click #(update-loop/inc-rate! !db 10)} "-"]
-      [:span (str (update-loop/get-rate !db) " ms")]
-      [:button {:on-click #(update-loop/inc-rate! !db -10)} "+"]
-      [:button {:on-click #(update-loop/inc-rate! !db -100)} "++"]]])
+    [:button.sidebar-toggle {:on-click #(sidebar/toggle! !db)} (if (sidebar/is-open? !db) "hide" "show") " patterns"]
+    [:button.undo {:on-click #(board/undo! !db)} "undo"]
+    [:button.pause {:on-click #(update-loop/toggle! !db)} (if (update-loop/is-enabled? !db) "pause" "play")]
+    [:button.reset {:on-click #(pattern-manager/use-selected-pattern! !db)} "reset"]
+    [:button.mm {:on-click #(update-loop/inc-rate! !db 100)} "--"]
+    [:button.m {:on-click #(update-loop/inc-rate! !db 10)} "-"]
+    [:div.rate (str (/ 1000 (update-loop/get-rate !db)) " fps")]
+    [:button.p {:on-click #(update-loop/inc-rate! !db -10)} "+"]
+    [:button.pp {:on-click #(update-loop/inc-rate! !db -100)} "++"]])
--- src/golem/ui/viewport/controls.cljs
+++ src/golem/ui/viewport/controls.cljs
@@ -12,15 +12,9 @@
 (defn component [!db]
   (let [!scale (cursor !db [:ui :viewport :scale])]
     [:div.viewport-controls
-      [:div.movers-label "Center: "]
-      [:div.movers
-        [:span "X,Y"]
-        [:button {:on-click #(shift-offset! !db [1 0])} "<"]
-        [:button {:on-click #(shift-offset! !db [0 1])} "^"]
-        [:button {:on-click #(shift-offset! !db [0 -1])} "v"]
-        [:button {:on-click #(shift-offset! !db [-1 0])} ">"]]
-      [:div.scalers-label "Zoom: "]
-      [:div.scalers
-        [:button {:on-click #(rescale! !db 0.9)} "-"]
-        [:span @!scale]
-        [:button {:on-click #(rescale! !db 1.1)} "+"]]]))
+      [:button.left {:on-click #(shift-offset! !db [1 0])} "<"]
+      [:button.up {:on-click #(shift-offset! !db [0 1])} "^"]
+      [:button.down {:on-click #(shift-offset! !db [0 -1])} "v"]
+      [:button.right {:on-click #(shift-offset! !db [-1 0])} ">"]
+      [:button.scale-down {:on-click #(rescale! !db 0.9)} "-"]
+      [:button.scale-up {:on-click #(rescale! !db 1.1)} "+"]]))
